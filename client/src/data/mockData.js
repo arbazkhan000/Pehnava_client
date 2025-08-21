06c12b9f775ac3d7d0ff1dd930dae585b0e8edf2
Arbazx01 <mdarbaz9675@gmail.com>
2025-08-22 00:20:15 +0530

    add mock file

---
+++ client/src/data/mockData.js
@@ -0,0 +1,11 @@
+export const girlsClothing = [
+    {
+        id: 1,
+        image: "https://hivadostore.com/cdn/shop/files/DSCF8377_00391ff4-153b-451a-b8d6-3c2188a2de05.jpg?v=1750853370&width=1440",
+    },
+    {
+        id: 2,
+        image: "https://hivadostore.com/cdn/shop/files/55ee5a9a01013e226f4b0e05e4077ec1.jpg?v=1750878563&width=1440",
+    },
+];
+
